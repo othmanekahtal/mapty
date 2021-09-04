43d08c64c56ab3c74f6006b9a1f1f5c8e1ef6d96
othmanekahtal <othmanekahtal@gmail.com>
2021-09-04 12:33:52 +0100

    add more code

--- script.js
+++ script.js
@@ -19,6 +19,16 @@ navigator.geolocation.getCurrentPosition(function (position) {
         alert('success get your current location!');
         const {latitude, longitude} = position.coords;
         console.log(latitude, longitude)
+        const coods = [latitude, longitude]
+        const map = L.map('map').setView(coods, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coods).addTo(map)
+            .bindPopup('your current location.')
+            .openPopup();
     },
     function () {
         window.alert('Error We can\'t get your current location !')
